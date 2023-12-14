962a4975d2dd7eac13815cd948643c645c0f8dd5
DhairyaSethi <me@derl.com>
2023-12-14 22:24:43 +0000

    upd(chore: expose hub addr) pushed from refs/heads/main

--- src/interfaces/hub/IService.sol
+++ src/interfaces/hub/IService.sol
@@ -5,4 +5,6 @@ interface IService {
     function onSubscribe(address subscriber, bytes32 topic) external;
 
     function onUnsubscribe(address subscriber, bytes32 topic) external;
+
+    function hub() external view returns (address);
 }
